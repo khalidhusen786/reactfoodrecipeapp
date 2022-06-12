d79f0fd805677572dd3a9c1ef8f7b96455b4c2e4
Khalid <khalidhusen000@gmail.com>
2022-06-13 01:39:17 +0530

    food category added in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,26 @@ function Header() {
         <div className='header'>
         <img src='./foodlogo.png' alt=''/>
       <input type="text" placeholder='Search Your Meal' ></input>
+      <div class="category">
+        <span>
+            <img src='./americanfood1.png' alt=''/>
+            <figcaption>American</figcaption>
+        </span>
+        <span>
+            <img src='./indianfood.png' alt=''/>
+            <figcaption>Indian</figcaption>
+            
+        </span>
+        <span>
+        <img src='./chinesefood.png' alt=''/>
+            <figcaption>Chinese</figcaption>
+        </span>
+        <span>
+        <img src='./nepalifood.png' alt=''/>
+            <figcaption>Nepali</figcaption>
+        </span>
+
+      </div>
       </div>
     </div>
   )
